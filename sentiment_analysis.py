099e527df30d39570b267fae61d4c6ff672c8d07
satojkovic <satojkovic@gmail.com>
2020-06-14 17:23:59 +0900

    Sentiment analysis of imdb_reviews

---
+++ sentiment_analysis.py
@@ -0,0 +1,63 @@
+import tensorflow_datasets as tfds
+import numpy as np
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+
+if __name__ == "__main__":
+    # 1. Load imdb dataset
+    imdb, info = tfds.load('imdb_reviews', with_info=True, as_supervised=True)
+
+    # 2. Split imdb dataset into train / test data
+    train_data, test_data = imdb['train'], imdb['test']
+
+    # 3. Prepare sentences and labels
+    training_sentences = []
+    training_labels = []
+
+    testing_sentences = []
+    testing_labels = []
+
+    for s, l in train_data:
+        training_sentences.append(str(s.numpy()))
+        training_labels.append(l.numpy())
+
+    for s, l in test_data:
+        testing_sentences.append(str(s.numpy()))
+        testing_labels.append(l.numpy())
+
+    training_labels = np.array(training_labels)
+    testing_labels = np.array(testing_labels)
+
+    # 4. Hyperparameters
+    vocab_size = 10000
+    embedding_dim = 16
+    max_length = 120
+    trunc_type = 'post'
+    oov_token = '<OOV>'
+
+    # 5. Tokenize
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+    tokenizer.fit_on_texts(training_sentences)
+    word_index = tokenizer.word_index
+    sequences = tokenizer.texts_to_sequences(training_sentences)
+    padded = pad_sequences(sequences, maxlen=max_length, truncating=trunc_type)
+
+    testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+    testing_padded = pad_sequences(testing_sequences, maxlen=max_length, truncating=trunc_type)
+
+    # 6. Defining model
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(6, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    # 7. Compile and training
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+
+    num_epochs = 10
+    model.fit(padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels))
