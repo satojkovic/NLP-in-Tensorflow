267d36ca6db920cd83f5271effbcc85bee8ac460
satojkovic <satojkovic@gmail.com>
2020-07-04 16:48:39 +0900

    Count the words frequency using CountVectorizer

--- eda_and_vis.py
+++ eda_and_vis.py
@@ -1,6 +1,8 @@
 import tensorflow_datasets as tfds
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 if __name__ == "__main__":
     # 1. Load imdb dataset
@@ -30,8 +32,10 @@ if __name__ == "__main__":
     # 4. Count the length of sentences
     all_sentences = training_sentences + testing_sentences
     all_labels = training_labels + testing_labels
-    positive_sentence_length = [len(sentence.split(' ')) for sentence, label in zip(all_sentences, all_labels) if label == 1]
-    negative_sentence_length = [len(sentence.split(' ')) for sentence, label in zip(all_sentences, all_labels) if label == 0]
+    positive_sentences = [sentence for sentence, label in zip(all_sentences, all_labels) if label == 1]
+    negative_sentences = [sentence for sentence, label in zip(all_sentences, all_labels) if label == 0]
+    positive_sentence_length = [len(sentence.split(' ')) for sentence in positive_sentences]
+    negative_sentence_length = [len(sentence.split(' ')) for sentence in negative_sentences]
     max_length = max(max(positive_sentence_length), max(negative_sentence_length))
 
     # 5. Plot the distribution
@@ -42,3 +46,29 @@ if __name__ == "__main__":
     plt.legend()
     plt.show()
 
+    # 6. Count the number of words
+    def get_top_n_words(corpus, n=20):
+        vectorizer = CountVectorizer()
+        X = vectorizer.fit_transform(corpus)
+        X_sum = X.sum(axis=0)
+        words_freq = [(word, X_sum[0, idx]) for word, idx in vectorizer.vocabulary_.items()]
+        words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
+        return words_freq[:n]
+
+    top_n = 20
+    positive_words_freq = get_top_n_words(positive_sentences, top_n)
+    negative_words_freq = get_top_n_words(negative_sentences, top_n)
+
+    plt.subplots_adjust(wspace=0.5)
+
+    left = range(top_n)
+    plt.subplot(1, 2, 1)
+    plt.title('Positive')
+    plt.bar(left, [freq for _, freq in positive_words_freq], tick_label=[word for word, _ in positive_words_freq], color='r', width=0.6)
+    plt.xticks(rotation=-90, fontsize=8)
+
+    plt.subplot(1, 2, 2)
+    plt.title('Negative')
+    plt.bar(left, [freq for _, freq in negative_words_freq], tick_label=[word for word, _ in negative_words_freq], color='b', width=0.6)
+    plt.xticks(rotation=-90, fontsize=8)
+    plt.show()
